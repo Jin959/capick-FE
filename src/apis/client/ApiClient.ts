fe807cec4b71e7dd207bb2b3a44683e07ceecedd
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2024-01-23 03:00:15 +0900

    [CP-7] feat: axios 설정과 api 계층 분리 및 DTO 개발

---
+++ src/apis/client/ApiClient.ts
@@ -0,0 +1,17 @@
+import ApiResponse from "@/dto/ApiResponse";
+
+interface ApiClient {
+
+  get<T>(url: string): Promise<ApiResponse<T>>;
+
+  post<T, U>(url: string, data: U): Promise<ApiResponse<T>>;
+
+  delete<T>(url: string): Promise<ApiResponse<T>>;
+
+  patch<T, U>(url: string, data: U): Promise<ApiResponse<T>>;
+
+  put<T, U>(url: string, data: U): Promise<ApiResponse<T>>;
+
+}
+
+export default ApiClient;
