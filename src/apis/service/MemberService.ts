a0304a34733a46910b254fe082b5833298893bd5
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2024-02-01 02:20:30 +0900

    [CP-8] feat: 회원가입 유효성 검사 로직 개발

--- src/apis/service/MemberService.ts
+++ src/apis/service/MemberService.ts
@@ -4,9 +4,15 @@ import ApiClient from "@/apis/client/ApiClient";
 class MemberService {
 
   private readonly apiClient: ApiClient;
+  private readonly emailRegExp: RegExp;
+  private readonly passwordRegExp: RegExp;
+  private readonly nicknameRegExp: RegExp;
 
   private constructor() {
     this.apiClient = ApiConfig.apiClient();
+    this.emailRegExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/;
+    this.passwordRegExp = /^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*()?])(?=\S+$)[0-9a-zA-Z!@#$%^&*()?]{8,20}$/;
+    this.nicknameRegExp = /^(?=.*[.\w가-힇ぁ-ゔァ-ヴー々〆〤一-龥])(?=\S+$)[.\w가-힇ぁ-ゔァ-ヴー々〆〤一-龥]{1,20}$/;
   }
 
   public static create = (): MemberService => {
@@ -15,7 +21,30 @@ class MemberService {
 
   public getMember = (memberId: string) => {
     this.apiClient.get(memberId);
-  };
+  }
+
+  public isNotValidMember = (email: string, password: string, nickname: string, confirmPassword: string) => {
+    return email.length === 0 || password.length === 0 || nickname.length === 0
+      || this.isNotValidEmailAndNotEmpty(email) || this.isNotValidPasswordAndNotEmpty(password)
+      || this.isNotValidNicknameAndNotEmpty(nickname) || this.isNotPasswordConfirmAndNotEmpty(password, confirmPassword);
+  }
+
+  public isNotValidEmailAndNotEmpty = (email: string) => {
+    return !this.emailRegExp.test(email) && email.length > 0;
+  }
+
+  public isNotValidPasswordAndNotEmpty = (password: string) => {
+    return !this.passwordRegExp.test(password) && password.length > 0;
+  }
+
+  public isNotValidNicknameAndNotEmpty = (nickname: string) => {
+    return !this.nicknameRegExp.test(nickname) && nickname.length > 0;
+  }
+
+  public isNotPasswordConfirmAndNotEmpty = (password: string, confirmPassword: string) => {
+    return password !== confirmPassword && password.length > 0
+  }
+
 }
 
 export default MemberService;
--- src/pages/signup/index.page.tsx
+++ src/pages/signup/index.page.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {NextPage} from "next";
 import {Button, Text, Heading, Input, Box} from "@chakra-ui/react";
 import ArrowBack from "@/../public/icons/google-material-arrow_back.svg"
@@ -6,11 +6,25 @@ import {useRouter} from "next/router";
 import PageFlexContainer from "@/components/container/PageFlexContainer";
 import Image from "next/image";
 import FormContainer from "@/components/container/FormContainer";
+import MemberService from "@/apis/service/MemberService";
 
 const IndexPage: NextPage = () => {
 
   const router = useRouter();
   const nicknameInput = useRef<HTMLInputElement>(null);
+
+  const [nickname, setNickname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const memberService = useRef(MemberService.create()).current;
+
+  const showNicknameValidationError: boolean = memberService.isNotValidNicknameAndNotEmpty(nickname);
+  const showEmailValidationError: boolean = memberService.isNotValidEmailAndNotEmpty(email);
+  const showPasswordValidationError: boolean = memberService.isNotValidPasswordAndNotEmpty(password);
+  const showPasswordConfirmError: boolean = memberService.isNotPasswordConfirmAndNotEmpty(password, confirmPassword);
+  const disableSubmit: boolean = memberService.isNotValidMember(email, password, nickname, confirmPassword)
 
   useEffect(() => {
     nicknameInput.current?.focus();
@@ -37,45 +51,70 @@ const IndexPage: NextPage = () => {
           <Heading size="md" textAlign="center">회원가입</Heading>
           <Input
             placeholder="닉네임"
+            minLength={1}
             maxLength={20}
             ref={nicknameInput}
+            onChange={(event) => setNickname(event.target.value)}
           />
           <Text fontSize="sm">
             서비스에서 사용할 20자리 이하 닉네임을 입력해주세요.
           </Text>
-          <Text fontSize="sm" color="red.500">
-            특수문자는 마침표(.), 밑줄(_) 만 사용할 수 있습니다.
-          </Text>
+          {showNicknameValidationError &&
+            <Text fontSize="sm" color="red.500">
+              특수문자는 마침표(.), 밑줄(_) 만 사용할 수 있습니다.
+            </Text>
+          }
           <Box p="2"></Box>
           <Input
             placeholder="이메일"
             maxLength={320}
+            onChange={(event) => setEmail(event.target.value)}
           />
           <Text fontSize="sm">
             로그인에 사용할 이메일을 입력해주세요.
           </Text>
-          <Text fontSize="sm" color="red.500">
-            example@gmail.com 처럼 이메일 형식에 맞아야 해요.
-          </Text>
+          {showEmailValidationError &&
+            <Text fontSize="sm" color="red.500">
+              example@gmail.com 처럼 이메일 형식에 맞아야 해요.
+            </Text>
+          }
           <Box p="2"></Box>
           <Input
             placeholder="비밀번호"
             type="password"
             minLength={8}
             maxLength={20}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <Input
+            placeholder="비밀번호 확인"
+            type="password"
+            minLength={8}
+            maxLength={20}
+            onChange={(event) => setConfirmPassword(event.target.value)}
           />
           <Text fontSize="sm">
             띄어쓰기 없이 영문/숫자/특수문자 8~20 자리로 작성해주세요.
           </Text>
-          <Text fontSize="sm" color="red.500">
-            영문/숫자/특수문자를 모두 사용해야 합니다.
-          </Text>
-          <Text fontSize="sm" color="red.500">
-            특수문자는 !, @, #, $, %, ^, &, *, (, ), ? 를 사용할 수 있습니다.
-          </Text>
+          {showPasswordValidationError &&
+            <>
+              <Text fontSize="sm" color="red.500">
+                영문/숫자/특수문자를 모두 사용해야 합니다.
+              </Text>
+              <Text fontSize="sm" color="red.500">
+                특수문자는 !, @, #, $, %, ^, &, *, (, ), ? 를 사용할 수 있습니다.
+              </Text>
+            </>
+          }
+          {showPasswordConfirmError &&
+            <Text fontSize="sm" color="red.500">
+              비밀번호가 일치하지 않습니다.
+            </Text>
+          }
           <Button
             colorScheme="brand"
-            onClick={handleOnSubmit}
+            type="submit"
+            isDisabled={disableSubmit && true}
           >
             시작하기
           </Button>
