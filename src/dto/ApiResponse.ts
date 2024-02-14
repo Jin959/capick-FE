e3097351dcdb0379effef5b50a69295dfd0f9c97
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2024-02-14 20:38:36 +0900

    [CP-7] feat: 공통 응답 DTO 타입 검사를 위해 type predicate 개발

--- src/dto/ApiResponse.ts
+++ src/dto/ApiResponse.ts
@@ -8,4 +8,11 @@ interface ApiResponse<T> {
 
 }
 
+export const isApiResponse = (object: object | unknown): object is ApiResponse<object | null> => {
+  return typeof object === "object"
+    && object !== null
+    && "code" in object
+    && "message" in object;
+}
+
 export default ApiResponse;
