1db0ae517ef917e845c79d9cd8063c26d287255a
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2024-02-15 16:06:25 +0900

    [CP-8] feat: 레이아웃 헤더 아바타 초기 개발

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,13 +1,15 @@
-import React from "react";
-import {Button, Flex} from "@chakra-ui/react";
+import React, {useContext} from "react";
+import {Avatar, Box, Button, Flex, Text} from "@chakra-ui/react";
 import {useRouter} from "next/router";
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "@/../public/images/capick-logo-en.svg";
+import {MemberContext} from "@/contexts/member";
 
 const Header = () => {
 
   const router = useRouter();
+  const member = useContext(MemberContext);
 
   return (
     <Flex
@@ -24,12 +26,23 @@ const Header = () => {
           alt="CaPick"
         />
       </Link>
-      <Button
-        variant="solid"
-        onClick={() => router.push("/login")}
-      >
-        로그인
-      </Button>
+      {member.id === 0 ? (
+        <Button
+          variant="solid"
+          onClick={() => router.push("/login")}
+        >
+          로그인
+        </Button>
+      ) : (
+        <Flex
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Text>{member.nickname}</Text>
+          <Box p="2"/>
+          <Avatar/>
+        </Flex>
+      )}
     </Flex>
   );
 };
