9d7f1c96f352d7ddc6a465c0113f65c286cbc639
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2024-02-16 02:09:08 +0900

    [CP-8] feat: 회원 컨텍스트 profile 상태 추가

--- src/contexts/member.tsx
+++ src/contexts/member.tsx
@@ -3,12 +3,22 @@ import {createContext, Dispatch, ReactElement, useReducer} from "react";
 interface State {
   id: number;
   nickname: string;
+  profile?: {
+    imageUrl?: string;
+    introduction?: string;
+  }
 }
 
 type Action = {
   type: "SET_MEMBER";
   id: number;
   nickname: string;
+} | {
+  type: "SET_PROFILE"
+  profile?: {
+    imageUrl?: string;
+    introduction?: string;
+  }
 }
 
 interface Props {
@@ -28,6 +38,15 @@ const reducer = (state: State, action: Action): State => {
         ...state,
         id: action.id,
         nickname: action.nickname,
+      }
+    case "SET_PROFILE":
+      return {
+        ...state,
+        profile: {
+          ...state.profile,
+          imageUrl: action.profile?.imageUrl,
+          introduction: action.profile?.introduction,
+        }
       }
     default:
       throw new Error("존재하지 않는 액션입니다.");
