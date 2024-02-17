2f3c34a5acda750f9f1eb406d3a92072a8f14471
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2024-02-18 00:08:27 +0900

    [CP-8] refactor: 회원 가입 페이지 input 컴포넌트 분리

---
+++ src/components/input/InputWithValidation.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {Input, Text} from "@chakra-ui/react";
+
+interface Props {
+  name?: string;
+  type?: string;
+  placeholder?: string;
+  minLength?: number;
+  maxLength?: number;
+  inputRef?: React.RefObject<HTMLInputElement>;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  validation: boolean;
+  validationErrorMessages: Array<string>;
+}
+
+interface ValidationError {
+  id: number;
+  message: string;
+}
+
+const createValidationErrorsWithId = (validationErrorMessages: Array<string>) => {
+  let id = 1;
+  return validationErrorMessages.map(
+    (validationErrorMessage) => ({
+      id: id++,
+      message: validationErrorMessage
+    })
+  );
+}
+
+const InputWithValidation = (props: Props) => {
+
+  const validationErrors: Array<ValidationError> = createValidationErrorsWithId(props.validationErrorMessages);
+
+  return (
+    <>
+      <Input
+        name={props.name}
+        type={props.type}
+        placeholder={props.placeholder}
+        minLength={props.minLength}
+        maxLength={props.maxLength}
+        ref={props.inputRef}
+        onChange={props.onChange}
+      />
+      {props.validation &&
+        validationErrors.map(validationError =>
+          <Text
+            key={validationError.id}
+            fontSize="sm"
+            color="red.500"
+          >
+            {validationError.message}
+          </Text>)
+      }
+    </>
+  );
+};
+
+export default InputWithValidation;
--- src/pages/signup/index.page.tsx
+++ src/pages/signup/index.page.tsx
@@ -1,6 +1,6 @@
 import React, {useContext, useEffect, useRef, useState} from 'react';
 import {NextPage} from "next";
-import {Button, Text, Heading, Input, Box} from "@chakra-ui/react";
+import {Button, Text, Heading, Box} from "@chakra-ui/react";
 import ArrowBack from "@/../public/icons/google-material-arrow_back.svg"
 import {useRouter} from "next/router";
 import PageFlexContainer from "@/components/container/PageFlexContainer";
@@ -8,6 +8,7 @@ import Image from "next/image";
 import FormContainer from "@/components/container/FormContainer";
 import MemberService from "@/apis/service/MemberService";
 import {MemberDispatchContext} from "@/contexts/member";
+import InputWithValidation from "@/components/input/InputWithValidation";
 
 const Signup: NextPage = () => {
 
@@ -74,72 +75,65 @@ const Signup: NextPage = () => {
           onSubmit={handleOnSubmit}
         >
           <Heading size="md" textAlign="center">회원가입</Heading>
-          <Input
+          <Text fontSize="sm">
+            서비스에서 사용할 20자리 이하 닉네임을 입력해주세요.
+          </Text>
+          <InputWithValidation
             name="nickname"
             placeholder="닉네임"
             minLength={1}
             maxLength={20}
-            ref={nicknameInput}
+            inputRef={nicknameInput}
             onChange={handleOnChange}
+            validation={showNicknameValidationError}
+            validationErrorMessages={[
+              "특수문자는 마침표(.), 밑줄(_) 만 사용할 수 있습니다."
+            ]}
           />
-          <Text fontSize="sm">
-            서비스에서 사용할 20자리 이하 닉네임을 입력해주세요.
-          </Text>
-          {showNicknameValidationError &&
-            <Text fontSize="sm" color="red.500">
-              특수문자는 마침표(.), 밑줄(_) 만 사용할 수 있습니다.
-            </Text>
-          }
           <Box p="2"></Box>
-          <Input
+          <Text fontSize="sm">
+            로그인에 사용할 이메일을 입력해주세요.
+          </Text>
+          <InputWithValidation
             name="email"
             placeholder="이메일"
             maxLength={320}
             onChange={handleOnChange}
+            validation={showEmailValidationError}
+            validationErrorMessages={[
+              "example@gmail.com 처럼 이메일 형식에 맞아야 해요."
+            ]}
           />
-          <Text fontSize="sm">
-            로그인에 사용할 이메일을 입력해주세요.
-          </Text>
-          {showEmailValidationError &&
-            <Text fontSize="sm" color="red.500">
-              example@gmail.com 처럼 이메일 형식에 맞아야 해요.
-            </Text>
-          }
           <Box p="2"></Box>
-          <Input
-            name="password"
-            placeholder="비밀번호"
-            type="password"
-            minLength={8}
-            maxLength={20}
-            onChange={handleOnChange}
-          />
-          <Input
-            name="confirmPassword"
-            placeholder="비밀번호 확인"
-            type="password"
-            minLength={8}
-            maxLength={20}
-            onChange={handleOnChange}
-          />
           <Text fontSize="sm">
-            띄어쓰기 없이 영문/숫자/특수문자 8~20 자리로 작성해주세요.
+            로그인에 사용할 비밀번호를 입력해주세요.
           </Text>
-          {showPasswordValidationError &&
-            <>
-              <Text fontSize="sm" color="red.500">
-                영문/숫자/특수문자를 모두 사용해야 합니다.
-              </Text>
-              <Text fontSize="sm" color="red.500">
-                특수문자는 !, @, #, $, %, ^, &, *, (, ), ? 를 사용할 수 있습니다.
-              </Text>
-            </>
-          }
-          {showPasswordConfirmError &&
-            <Text fontSize="sm" color="red.500">
-              비밀번호가 일치하지 않습니다.
-            </Text>
-          }
+          <InputWithValidation
+            name="password"
+            type="password"
+            placeholder="비밀번호"
+            minLength={8}
+            maxLength={20}
+            onChange={handleOnChange}
+            validation={showPasswordValidationError}
+            validationErrorMessages={[
+              "영문/숫자/특수문자를 모두 사용해야 합니다.",
+              "띄어쓰기 없이 영문/숫자/특수문자 8~20 자리로 작성해주세요.",
+              "특수문자는 !, @, #, $, %, ^, &, *, (, ), ? 를 사용할 수 있습니다."
+            ]}
+          />
+          <InputWithValidation
+            name="confirmPassword"
+            type="password"
+            placeholder="비밀번호 확인"
+            minLength={8}
+            maxLength={20}
+            onChange={handleOnChange}
+            validation={showPasswordConfirmError}
+            validationErrorMessages={[
+              "비밀번호가 일치하지 않습니다."
+            ]}
+          />
           <Button
             colorScheme="brand"
             type="submit"
