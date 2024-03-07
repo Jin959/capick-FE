0f993b2899db997843b0ba1e1006891e290d6426
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2024-03-07 18:00:26 +0900

    [CP-27] feat: SEO 헤드 개발

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -5,16 +5,20 @@ import theme from "@/styles/theme";
 import globalStyle from "@/styles/global";
 import Layout from "@/components/common/Layout";
 import MemberProvider from "@/contexts/member";
+import SeoHead from "@/components/common/SeoHead";
 
 export default function App({Component, pageProps}: AppProps) {
   return (
-    <ChakraProvider resetCSS={true} theme={theme}>
-      <Global styles={globalStyle}/>
-      <MemberProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </MemberProvider>
-    </ChakraProvider>
+    <>
+      <SeoHead/>
+      <ChakraProvider resetCSS={true} theme={theme}>
+        <Global styles={globalStyle}/>
+        <MemberProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </MemberProvider>
+      </ChakraProvider>
+    </>
   );
 }
