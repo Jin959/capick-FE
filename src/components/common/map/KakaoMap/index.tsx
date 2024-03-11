38e7fd754c81c1a2dc6c264994671bc132e22b00
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2024-03-11 17:34:31 +0900

    [CP-27] feat: 현재 좌표를 카카오 지도에 연동

--- src/components/common/map/KakaoMap/index.tsx
+++ src/components/common/map/KakaoMap/index.tsx
@@ -7,18 +7,38 @@ declare global {
   }
 }
 
+interface Position {
+  latitude: number;
+  longitude: number;
+}
+
 const KakaoMap = () => {
 
   const mapRef = useRef<HTMLDivElement>(null);
-
-  const lat = 37.5642135;
-  const lon = 127.0016985;
+  const currentPosition = useRef<Position>({
+    latitude: 37.571648599,
+    longitude: 126.976372775
+  });
 
   useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position: GeolocationPosition) => {
+          currentPosition.current = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          };
+        },
+        () => {
+          window.alert("기기 및 브라우저 위치 접근 권한을 켜주세요!");
+        }
+      );
+    }
+
     window.kakao.maps.load(() => {
       const {kakao} = window;
       const mapOption = {
-        center: new kakao.maps.LatLng(lat, lon),
+        center: new kakao.maps.LatLng(currentPosition.current.latitude, currentPosition.current.longitude),
         level: 3
       };
       const map = new kakao.maps.Map(mapRef.current, mapOption);
