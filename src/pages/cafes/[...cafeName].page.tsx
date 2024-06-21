1da77bacfbe6eb5b517f851f638de1dee1633af7
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2024-06-21 21:01:21 +0900

    [CP-31] feat: 카페 별 리뷰 목록 페이지 생성과 라우팅 정보 퍼블리싱

---
+++ src/pages/cafes/[...cafeName].page.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {NextPage} from "next";
+import {useRouter} from "next/router";
+import BackButton from "@/components/common/button/BackButton";
+import PageFlexContainer from "@/components/common/container/PageFlexContainer";
+import {Button, Heading, ListItem, UnorderedList} from "@chakra-ui/react";
+import BoxContainer from "@/components/common/container/BoxContainer";
+
+const CafeDetail : NextPage = () => {
+  const router = useRouter();
+  const [cafeName, kakaoPlaceId] = (router.query.cafeName ?? []) as Array<string>;
+
+  return (
+    <>
+      <BackButton/>
+      <PageFlexContainer>
+        <Heading size="md" textAlign="center">
+          {cafeName}
+        </Heading>
+        <BoxContainer>
+          <UnorderedList>
+            <ListItem>주소 : {router.query.address ?? "로딩 중"}</ListItem>
+            <ListItem>도로명 : {router.query.roadAddress ?? "로딩 중"}</ListItem>
+          </UnorderedList>
+          <Button
+            colorScheme="brand"
+            onClick={() => router.push((router.query.detailPageUrl ?? "/") as string)}
+          >
+            상세 정보 및 길찾기
+          </Button>
+        </BoxContainer>
+        {/* TODO: 리뷰 목록 개발하기*/}
+      </PageFlexContainer>
+    </>
+  );
+};
+
+export default CafeDetail;
