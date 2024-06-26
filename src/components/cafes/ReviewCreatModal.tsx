4f065236dbcc729042f410e4bb828e4448c69919
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2024-06-26 17:55:37 +0900

    [CP-31] feat: 리뷰 생성 모달 리뷰 설문과 리뷰 내용 작성 조건부 렌더링

--- src/components/cafes/ReviewCreatModal.tsx
+++ src/components/cafes/ReviewCreatModal.tsx
@@ -8,7 +8,7 @@ import {
   ModalHeader,
   ModalOverlay
 } from "@chakra-ui/modal";
-import {Button, Input} from "@chakra-ui/react";
+import {Button, Input, Textarea} from "@chakra-ui/react";
 import reviewConstant from "@/constants/reviewConstant";
 import {createDataWithId} from "@/utils/func";
 import ReviewService from "@/apis/service/ReviewService";
@@ -37,11 +37,12 @@ const ReviewCreatModal = ({reviewService}: Props) => {
   };
 
   const handleOnClickNext = () => {
-    if (!reviewService.isSurveyEnd(surveyType)) {
-      setSurveyType(reviewService.getNextSurveyType(surveyType));
-    } else {
-      // setIsOpen(false);
-    }
+    setSurveyType(reviewService.getNextSurveyType(surveyType));
+  };
+
+  const handleOnClickDone = () => {
+    setIsOpen(false);
+    setSurveyType(reviewService.getFirstSurvey());
   };
 
   return (
@@ -59,76 +60,96 @@ const ReviewCreatModal = ({reviewService}: Props) => {
         scrollBehavior="inside"
       >
         <ModalOverlay/>
-        <ModalContent>
-          <ModalCloseButton/>
-          {showContentTextarea ? (
-            <>
-              <ModalHeader
-                m="0 auto"
+        {showContentTextarea ? (
+          <ModalContent>
+            <ModalCloseButton/>
+            <ModalHeader
+              m="0 auto"
+            >
+              한마디
+            </ModalHeader>
+            <ModalBody>
+              <Textarea
+                placeholder="리뷰 내용 입력"
+                minH="250"
+                resize="none"
+              />
+            </ModalBody>
+            <ModalFooter
+              display="flex"
+              justifyContent="space-around"
+              alignItems="center"
+            >
+              <Button
+                onClick={handleOnClickBefore}
               >
-                한마디
-              </ModalHeader>
-              {/*// TODO: 리뷰 내용 작성 기능 개발하기, 이 부분만 다르게 할지 아예 다른 모달로 분리할지 고민하기*/}
-              <ModalBody>
-              </ModalBody>
-            </>
-          ) : (
-            <>
-              <ModalHeader
-                m="0 auto"
+                이전
+              </Button>
+              <Button
+                variant='ghost'
+                onClick={handleOnClickDone}
               >
-                {reviewConstant.survey.question[surveyType]}
-              </ModalHeader>
-              <ModalBody
-                display="flex"
-                flexDirection="column"
-                justifyContent="space-around"
-                alignItems="center"
-              >
-                {
-                  surveyOptions.map(option =>
-                    <Button
-                      key={option.id}
-                      m="1"
-                      w="90%"
-                      colorScheme="subBrand"
-                      color="black"
-                    >
-                      {option.data}
-                    </Button>)
-                }
-                {showDirectInputPlaceholder &&
-                  <Input
-                    placeholder={reviewConstant.survey.directInputPlaceholder[surveyType]}
-                    _placeholder={{opacity: 1, color: "black", fontWeight: "bold", textAlign: "center"}}
+                완료
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        ) : (
+          <ModalContent>
+            <ModalCloseButton/>
+            <ModalHeader
+              m="0 auto"
+            >
+              {reviewConstant.survey.question[surveyType]}
+            </ModalHeader>
+            <ModalBody
+              display="flex"
+              flexDirection="column"
+              justifyContent="space-around"
+              alignItems="center"
+            >
+              {
+                surveyOptions.map(option =>
+                  <Button
+                    key={option.id}
                     m="1"
                     w="90%"
-                    bg="subBrand.500"
-                    variant="filled"
-                    focusBorderColor="brand.main"
-                  />
-                }
-              </ModalBody>
-            </>
-          )}
-          <ModalFooter
-            display="flex"
-            justifyContent="space-around"
-            alignItems="center"
-          >
-            <Button
-              onClick={handleOnClickBefore}
+                    colorScheme="subBrand"
+                    color="black"
+                  >
+                    {option.data}
+                  </Button>)
+              }
+              {showDirectInputPlaceholder &&
+                <Input
+                  placeholder={reviewConstant.survey.directInputPlaceholder[surveyType]}
+                  _placeholder={{opacity: 1, color: "black", fontWeight: "bold", textAlign: "center"}}
+                  m="1"
+                  w="90%"
+                  bg="subBrand.500"
+                  variant="filled"
+                  focusBorderColor="brand.main"
+                />
+              }
+            </ModalBody>
+            <ModalFooter
+              display="flex"
+              justifyContent="space-around"
+              alignItems="center"
             >
-              이전
-            </Button>
-            <Button
-              variant='ghost'
-              onClick={handleOnClickNext}
-            >
-              다음
-            </Button>
-          </ModalFooter>
-        </ModalContent>
+              <Button
+                onClick={handleOnClickBefore}
+              >
+                이전
+              </Button>
+              <Button
+                variant='ghost'
+                onClick={handleOnClickNext}
+              >
+                다음
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        )}
       </Modal>
     </>
   );
