46fae4670db45b93e1f2141f143767e079a62e00
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2024-06-26 17:42:41 +0900

    [CP-31] refactor: 리뷰 서비스에 리뷰 설문이 끝남을 알리는 상수 추가

--- src/apis/service/ReviewService.ts
+++ src/apis/service/ReviewService.ts
@@ -3,6 +3,8 @@ import ApiConfig from "@/apis/ApiConfig";
 
 class ReviewService {
 
+  private static readonly SURVEY_END: string = "end";
+
   private readonly apiClient: ApiClient;
   private readonly surveyTypes: Array<string>;
   private readonly surveyTypesWithDirectInput: Array<string>;
@@ -19,13 +21,13 @@ class ReviewService {
 
   public getNextSurveyType = (surveyType: string) => {
     const nextIndex = this.surveyTypes.indexOf(surveyType) + 1;
-    if (nextIndex >= this.surveyTypes.length) return "content";
+    if (nextIndex >= this.surveyTypes.length) return ReviewService.SURVEY_END;
     else return this.surveyTypes[nextIndex];
   }
 
   public getBeforeSurveyType = (surveyType: string) => {
     const beforeIndex = this.surveyTypes.indexOf(surveyType) - 1;
-    if (surveyType === "content") return this.surveyTypes[this.surveyTypes.length - 1];
+    if (surveyType === ReviewService.SURVEY_END) return this.surveyTypes[this.surveyTypes.length - 1];
     else if (beforeIndex < 0) return this.surveyTypes[0];
     else return this.surveyTypes[beforeIndex];
   }
@@ -35,7 +37,7 @@ class ReviewService {
   }
 
   public isSurveyEnd = (surveyType: string) => {
-    return surveyType === "content"
+    return surveyType === ReviewService.SURVEY_END;
   }
 
   public isNeedDirectInput = (surveyType: string) => {
