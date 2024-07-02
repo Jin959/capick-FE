5b19d1044e6badb476574e458eaeca4ba11f012a
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2024-07-02 17:09:56 +0900

    [CP-27] refactor: 위치 추적 버튼 프롭스 타입 선언 파스칼 케이스로 수정

--- src/components/common/map/KakaoMap/PositionTrackingButton.tsx
+++ src/components/common/map/KakaoMap/PositionTrackingButton.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Button} from "@chakra-ui/react";
-import mapService from "@/apis/service/MapService";
+import MapService from "@/apis/service/MapService";
 import {useRouter} from "next/router";
 import Image from "next/image";
 import PositionTrackingIcon from "@/../public/icons/google-material-my_location_FILL0_wght400_GRAD0_opsz24.svg"
 
 interface Props {
   zIndex: string;
-  mapService: mapService;
+  mapService: MapService;
 }
 
 const PositionTrackingButton = ({mapService, zIndex}: Props) => {
