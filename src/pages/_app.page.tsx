99f667fd1e3aa1239cc0e3b6847c740048fb63a7
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2024-07-06 00:45:04 +0900

    [CP-31] feat: 글로벌 컨텍스트 프로바이더 개발 후 모달 및 카페 컨텍스트와 적용

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -4,8 +4,11 @@ import {Global} from "@emotion/react";
 import theme from "@/styles/theme";
 import globalStyle from "@/styles/global";
 import Layout from "@/components/common/Layout";
-import MemberProvider from "@/contexts/member";
 import SeoHead from "@/components/common/SeoHead";
+import GlobalProvider from "@/contexts/GlobalProvider";
+import MemberProvider from "@/contexts/member";
+import ModalProvider from "@/contexts/modal";
+import CafeProvider from "@/contexts/cafe";
 
 export default function App({Component, pageProps}: AppProps) {
   return (
@@ -13,11 +16,11 @@ export default function App({Component, pageProps}: AppProps) {
       <SeoHead/>
       <ChakraProvider resetCSS={true} theme={theme}>
         <Global styles={globalStyle}/>
-        <MemberProvider>
+        <GlobalProvider contexts={[MemberProvider, ModalProvider, CafeProvider]}>
           <Layout>
             <Component {...pageProps} />
           </Layout>
-        </MemberProvider>
+        </GlobalProvider>
       </ChakraProvider>
     </>
   );
