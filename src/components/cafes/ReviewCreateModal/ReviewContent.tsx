41fcae32dbb2c4258fce312ea79f6bc9aae368db
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2024-07-18 20:03:16 +0900

    [CP-31] feat: 리뷰 작성 모달 내용 작성 컴포넌트에 이미지 업로드 컴포넌트 추가

--- src/components/cafes/ReviewCreateModal/ReviewContent.tsx
+++ src/components/cafes/ReviewCreateModal/ReviewContent.tsx
@@ -1,7 +1,7 @@
-import React, {useContext} from 'react';
+import React, {useContext, useRef, useState} from 'react';
 import {useRouter} from "next/router";
 import {ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader} from "@chakra-ui/modal";
-import {Button, Textarea} from "@chakra-ui/react";
+import {Button, Input, InputGroup, Textarea, Text, Box} from "@chakra-ui/react";
 import ReviewService from "@/apis/service/ReviewService";
 import {ReviewContext, ReviewDispatchContext} from "@/contexts/review";
 import reviewConstant from "@/constants/reviewConstant";
@@ -9,6 +9,8 @@ import {StringMap} from "@/types/common";
 import {CafeContext} from "@/contexts/cafe";
 import {MemberContext} from "@/contexts/member";
 import {ModalDispatchContext} from "@/contexts/modal";
+import Image from "next/image";
+import UploadImageIcon from "@/../public/icons/google-material-add_photo_alternate_FILL0_wght400_GRAD0_opsz24.svg"
 
 interface Props {
   reviewService: ReviewService;
@@ -17,6 +19,9 @@ interface Props {
 const ReviewContent = ({reviewService}: Props) => {
 
   const router = useRouter();
+
+  const imageInputRef = useRef<HTMLInputElement>(null);
+  const [images, setImages] = useState<Array<File>>([]);
 
   const member = useContext(MemberContext);
   const cafe = useContext(CafeContext);
@@ -31,7 +36,15 @@ const ReviewContent = ({reviewService}: Props) => {
     });
   }
 
-  const handleOnChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+  const handleOnClickUpload = (event: React.MouseEvent<HTMLDivElement>) => {
+    imageInputRef.current?.click();
+  }
+
+  const handleOnChangeImageInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setImages(Array.from(event.target.files as FileList));
+  }
+
+  const handleOnChangeTextarea = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     dispatchReview({
       type: "SET_CONTENT",
       content: event.target.value
@@ -90,13 +103,40 @@ const ReviewContent = ({reviewService}: Props) => {
           한마디
         </ModalHeader>
         <ModalBody>
+          <InputGroup
+            onClick={handleOnClickUpload}
+            minH="150"
+            display="flex"
+            flexDirection="column"
+            justifyContent="space-around"
+            alignItems="center"
+            border="dashed"
+            borderColor="gray.300"
+            borderRadius="md"
+          >
+            <Input
+              type="file"
+              multiple={true}
+              ref={imageInputRef}
+              onChange={handleOnChangeImageInput}
+              display="none"
+            />
+            <Image
+              src={UploadImageIcon}
+              alt="UploadImage"
+              width={50}
+            />
+            <Text>사진을 선택해주세요</Text>
+            <Button size="sm">사진 첨부</Button>
+          </InputGroup>
+          <Box p="2"/>
           <Textarea
             placeholder="리뷰 내용 입력"
-            minH="250"
+            minH="100"
             resize="none"
             maxLength={300}
             value={review.content}
-            onChange={handleOnChange}
+            onChange={handleOnChangeTextarea}
           />
         </ModalBody>
         <ModalFooter
