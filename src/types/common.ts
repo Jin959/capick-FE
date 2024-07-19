b6278d3c1d18616fc2c9047ba5a18cd5048b028a
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2024-07-19 16:58:27 +0900

    [CP-31] refactor: 리스트 렌더링을 위한 배열 원소의 타입을 글로벌 타입으로

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,3 +1,9 @@
 export interface StringMap<T> {
   [key: string]: T;
 }
+
+// TODO: utils.func 에서 가져온 리스트 렌더링을 위한 타입, 더 좋은 네이밍이나 WET 하게 사용하는 곳 마다 다시 적을지 생각하기
+export interface DataWithId<T> {
+  id: number;
+  data: T;
+}
--- src/utils/func.ts
+++ src/utils/func.ts
@@ -1,16 +1,11 @@
-import {StringMap} from "@/types/common";
-
-interface DataWithId<T> {
-  id: number;
-  data: T;
-}
+import {DataWithId, StringMap} from "@/types/common";
 
 export function createDataWithId<T>(data: Array<T>): Array<DataWithId<T>>;
 export function createDataWithId<T>(data: StringMap<T>, dataFrom?: "key" | "value"): Array<DataWithId<T | string>>;
 export function createDataWithId (data: any, dataFrom?: any): any {
 
   if (!data) {
-    console.log("렌더링 리스트 데이터를 생성하는 데 실패했습니다.");
+    console.log("리스트 렌더링 데이터를 생성하는 데 실패했습니다.");
     return [];
   }
 
