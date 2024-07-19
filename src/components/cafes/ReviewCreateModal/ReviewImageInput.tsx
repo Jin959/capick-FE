45a3a35c9b55c70a30d319ab3c5f48b9d240f061
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2024-07-19 17:57:32 +0900

    [CP-31] feat: 리뷰 작성 모달의 리뷰 이미지 인풋에 이미지 미리보기 기능 개발

--- src/components/cafes/ReviewCreateModal/ReviewImageInput.tsx
+++ src/components/cafes/ReviewCreateModal/ReviewImageInput.tsx
@@ -1,12 +1,16 @@
-import React, {useContext, useRef} from 'react';
-import {Button, Input, InputGroup, Text} from "@chakra-ui/react";
+import React, {useContext, useEffect, useRef, useState} from 'react';
+import {Button, Flex, Input, InputGroup, Text} from "@chakra-ui/react";
 import Image from "next/image";
 import UploadImageIcon from "@/../public/icons/google-material-add_photo_alternate_FILL0_wght400_GRAD0_opsz24.svg"
 import {ReviewContext, ReviewDispatchContext} from "@/contexts/review";
+import {createDataWithId} from "@/utils/func";
+import {DataWithId} from "@/types/common";
 
 const ReviewImageInput = () => {
 
   const imageInputRef = useRef<HTMLInputElement>(null);
+
+  const [imagePreviews, setImagePreviews] = useState<Array<DataWithId<string>>>([]);
 
   const review = useContext(ReviewContext);
   const dispatchReview = useContext(ReviewDispatchContext);
@@ -19,14 +23,23 @@ const ReviewImageInput = () => {
     dispatchReview({
       type: "SET_IMAGES",
       images: Array.from(event.target.files as FileList)
-    })
+    });
   }
+
+  useEffect(() => {
+    setImagePreviews(createDataWithId(
+      review.images.map(image => URL.createObjectURL(image))
+    ));
+    return () => {
+      imagePreviews.map(preview => URL.revokeObjectURL(preview.data));
+    };
+  }, [review.images]);
 
   return (
     <>
       <InputGroup
         onClick={handleOnClick}
-        minH="150"
+        minH="15rem"
         display="flex"
         flexDirection="column"
         justifyContent="space-around"
@@ -38,17 +51,45 @@ const ReviewImageInput = () => {
         <Input
           type="file"
           multiple={true}
+          accept="image/*"
           ref={imageInputRef}
           onChange={handleOnChange}
           display="none"
         />
-        <Image
-          src={UploadImageIcon}
-          alt="UploadImage"
-          width={50}
-        />
-        <Text>사진을 선택해주세요</Text>
-        <Button size="sm">사진 첨부</Button>
+        {review.images.length === 0 ? (
+          <>
+            <Image
+              src={UploadImageIcon}
+              alt="UploadImage"
+              width={50}
+            />
+            <Text>사진을 선택해주세요</Text>
+            <Button size="sm">사진 선택</Button>
+          </>
+        ) : (
+          <>
+            <Flex
+              justifyContent="space-around"
+              alignItems="center"
+              overflow="auto"
+            >
+              {imagePreviews.map(
+                preview => <Image
+                  key={preview.id}
+                  src={preview.data}
+                  alt={`ReviewImage${preview.id}`}
+                  width={150}
+                  height={150}
+                  style={{
+                    margin: "0 0.2rem 0 0.2rem",
+                    maxHeight: "10rem",
+                    objectFit: "contain"
+                  }}
+                />)}
+            </Flex>
+            <Button size="sm">사진 바꾸기</Button>
+          </>
+        )}
       </InputGroup>
     </>
   );
