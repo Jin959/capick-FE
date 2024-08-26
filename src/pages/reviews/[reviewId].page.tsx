6399a381d48bf010e4fd35c0aa4108c2e2f1e493
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2024-08-26 23:56:47 +0900

    [CP-32] feat: 리뷰 상세 페이지 스트링 정보 퍼블리싱

--- src/pages/reviews/[reviewId].page.tsx
+++ src/pages/reviews/[reviewId].page.tsx
@@ -3,6 +3,7 @@ import BackButton from "@/components/common/button/BackButton";
 import PageFlexContainer from "@/components/common/container/PageFlexContainer";
 import {useRouter} from "next/router";
 import useReviewService from "@/hooks/service/useReivewService";
+import {Avatar, Box, Divider, Flex, Tag, Text} from "@chakra-ui/react";
 
 const ReviewPage = () => {
   const router = useRouter();
@@ -47,7 +48,43 @@ const ReviewPage = () => {
     <>
       <BackButton/>
       <PageFlexContainer>
-
+        <Flex
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Flex
+            alignItems="center"
+          >
+            <Avatar src={writer.profileImageUrl}/>
+            <Box p="2"/>
+            <Text as='b'>
+              {writer.nickname}
+            </Text>
+          </Flex>
+          <Text>
+            {review.registeredAt}
+          </Text>
+        </Flex>
+        <Divider/>
+        <Box>
+          <Tag
+            w="fit-content"
+            mr="2"
+            mb="1"
+            colorScheme="brand"
+          >
+            {review.menu}
+          </Tag>
+          <Tag
+            w="fit-content"
+          >
+            {review.visitPurpose}
+          </Tag>
+        </Box>
+        <Divider/>
+        <Text>
+          {review.content}
+        </Text>
       </PageFlexContainer>
     </>
   );
